337bfb7c42dca9d7d7f21992f2274b7b43402589
Prianti Banik <59369461+prianti29@users.noreply.github.com>
2023-09-22 17:00:00 +0600

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,50 +16,59 @@ mix.js('resources/js/app.js', 'public/js')
         //
     ]);
 
- // public function store(Request $request)
- //  {
+ public function store(Request $request)
+{
+    $input = $request->all();
+    
+    // Assuming the data is passed in an array named 'entries'
+    $entries = json_decode($input['form'], true);
 
-// $input = $request->all();
+    // Validate that 'entries' is an array and not empty
+    if (!is_array($entries) || empty($entries)) {
+        $status = [
+            "status" => 406,
+            "msg" => "Entries are required and should be an array."
+        ];
+        return $status;
+    }
 
-    // parse_str($input['form'], $data);
-    // for($i = 0; $i < count($data['field_value']); $i++)
-    // {
-    //   $type = isset($data['type'][$i])? trim($data['type'][$i]):"0";
-    //   $field = isset($data['field'][$i])? trim($data['field'][$i]):"0";
-    //   $field_value = isset($data['field_value'][$i])? trim($data['field_value'][$i]):"0";
-    //   $month = isset($data['month'][$i])? trim($data['month'][$i]):"0";
-    //   $year = isset($data['year'][$i])? trim($data['year'][$i]):"0";
-    // }
+    $insertedData = [];
 
-    //   if(@empty($data['month'])){
-    //         $status = array(
-    //             "status"=>406,
-    //             "msg"=>"Month is required!"
-    //             );
-    //             return $status;
-    //       }
-    //   if(@empty($data['year'])){
-    //     $status = array(
-    //         "status"=>406,
-    //         "msg"=>"Year is required!"
-    //         );
-    //         return $status;
-    //   }
-    //   $DBstatus = DB::table('mm_demography')->insert(
-    //     array( 
-    //         'type' => $type,            
-    //         'field' => $field,
-    //         'field_value' => $field_value, 
-    //         'month' => $month,
-    //         'year' => $year,           
-    //       )    
-    //   );
-        
-    //   $this->recordEntryModifyDate(30);
-          
-    //   if($DBstatus){                
-    //     return $status = array(
-    //       "status"=>200,
-    //       "msg"=>"Major entry has bee successfully updated"
-    //     );
-    //   }  
+    foreach ($entries as $entry) {
+        $type = isset($entry['type']) ? trim($entry['type']) : "0";
+        $field = isset($entry['field']) ? trim($entry['field']) : "0";
+        $field_value = isset($entry['field_value']) ? trim($entry['field_value']) : "0";
+        $month = isset($entry['month']) ? trim($entry['month']) : "0";
+        $year = isset($entry['year']) ? trim($entry['year']) : "0";
+
+        // Insert each entry into the database
+        $DBstatus = DB::table('mm_demography')->insert([
+            'type' => $type,
+            'field' => $field,
+            'field_value' => $field_value,
+            'month' => $month,
+            'year' => $year,
+        ]);
+
+        if ($DBstatus) {
+            $insertedData[] = $entry;
+        }
+    }
+
+    $this->recordEntryModifyDate(30);
+
+    if (!empty($insertedData)) {
+        $status = [
+            "status" => 200,
+            "msg" => "Entries have been successfully updated",
+            "insertedData" => $insertedData
+        ];
+        return $status;
+    } else {
+        $status = [
+            "status" => 406,
+            "msg" => "Failed to insert entries into the database"
+        ];
+        return $status;
+    }
+}
